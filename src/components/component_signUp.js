fb8925cf77697d5bd9b2091ed0da61ba2d04c4e2
koabc9999 <koabc9999@gmail.com>
2022-01-23 17:40:46 +0900

    sign up button can post request to server

--- src/components/component_signUp.js
+++ src/components/component_signUp.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 import cat from '../images/cat.jpg';
 
@@ -56,6 +57,41 @@ const StyledLink = styled(Link)`
 `;
 
 function ComponentSignUp() {
+    const [newAccount, setNewAccount] = useState({// state를 선언해주는데 object 행태로 해줌
+        name: "",
+        id: "",
+        password: "",
+        pwcheck: "",
+    });
+
+    const onChangeAccount = (e) => {// 내부의 정보가 바뀌었을 때 state를 바꿔주도록 실행하는 함수
+        setNewAccount({
+            ...newAccount,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const onButtonPressed = (e) => {// 회원가입 버튼이 눌렸을때 실행되는 함수
+        console.log(`${newAccount.name}, ${newAccount.id}, ${newAccount.password}, ${newAccount.pwcheck}`);// 입력이 어떤게 들어왔는지 콘솔에 띄워줌
+        if(newAccount.name === "" || newAccount.id === "" || newAccount.password === "" || newAccount.pwcheck === "") {// 비어있는 칸은 없는지 확인
+            alert("모든 칸을 채워넣어야 합니다!");
+        }
+        else if(newAccount.password !== newAccount.pwcheck) {// 비밀번호와 비밀번호 확인이 같은지 확인
+            alert("비밀번호가 동일하지 않습니다!");
+        }
+        else {// 입력이 온전하게 잘 되어서 회원가입이 완료됨
+            axios.post('/user_inform/onSignUp', null, {
+                params: {// 넘겨줄 정보로 param이라는 객체를 넘겨줌
+                    'user_name': newAccount.name,
+                    'user_id': newAccount.id,
+                    'user_pw': newAccount.password
+                }
+            })
+            .then(res => {// 서버에서 온 응답을 res에 넣고 이후 코드를 실행해줌
+                console.log(res);
+            });
+        }
+    }
 
     return (
         <Container>
@@ -66,6 +102,7 @@ function ComponentSignUp() {
                 name="name"
                 type="text"
                 placeholder="이름"
+                onChange={onChangeAccount}
             />
             <StyledText>아이디</StyledText>
             <Input
@@ -73,6 +110,7 @@ function ComponentSignUp() {
                 name="id"
                 type="text"
                 placeholder="아이디"
+                onChange={onChangeAccount}
             />
             <StyledText>비밀번호</StyledText>
             <Input
@@ -80,14 +118,17 @@ function ComponentSignUp() {
                 name="password"
                 type="password"
                 placeholder="비밀번호"
+                onChange={onChangeAccount}
             />
+            <StyledText>비밀번호 확인</StyledText>
             <Input
                 id="pwcheck"
                 name="pwcheck"
                 type="password"
                 placeholder="비밀번호 확인"
+                onChange={onChangeAccount}
             />
-            <Button>회원가입 요청</Button>
+            <Button onClick = {onButtonPressed}>회원가입 요청</Button>
             <StyledLink to="/">로그인으로 돌아가기</StyledLink>
         </Container>
     );
