9c902f078f45c961724b0efe2b01107ce371955f
koabc9999 <koabc9999@gmail.com>
2022-01-23 17:40:00 +0900

    screen file with components

--- src/screen/screen_login.js
+++ src/screen/screen_login.js
@@ -5,8 +5,8 @@ import ComponentLogin from '../components/component_login.js';
 import ComponentSignUp from '../components/component_signUp.js';
 
 
-function ScreenLogin() {
-
+function ScreenLogin() {// javascript 프레임워크, npm  node project manager
+    
     
     //<img src="./images/pepe.jpg" />
     return (
